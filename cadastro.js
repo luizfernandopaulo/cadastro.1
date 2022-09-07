f653d4a2afe7ff9a6cc24b8cf2ac22ab7814621f
LUIZ FERNANDO DE PAULO GOMES FERREIRA <102976496+luizfernandopaulo@users.noreply.github.com>
2022-09-06 21:53:13 -0300

    Add files via upload

---
+++ cadastro.js
@@ -0,0 +1,32 @@
+/*let btnEnviar = document.querySelectorAll('#botoes button')
+let btnExcluir = document.querySelectorAll('#botoes button')
+let btnEditar = document.querySelectorAll('#butoes button')
+
+
+let nome =document.querySelectorAll('#wrap input')
+let quantidade=document.querySelectorAll('#wrap input')
+
+
+
+let tabela = document.querySelectorAll('#saida tabela')
+let bd=[]
+
+
+btnEnviar.onclick=function(){
+    let produto = new Object();
+    produto.nome=nome.value;
+    produto.quantidade=quantidade.value;
+    produto.preco=preco.value;
+    produto.prateleira=prateleira.value;
+    produto.descricao=descricao.value;
+    produto.categoria=categoria.value;
+    produto.id=bd.length;
+    bd.push(produto)
+    tabela.innerHTML=`<tr><td><input type"checkbox"id="${produto.id}" `
+}
+
+btnExcluir.onclick=function(){
+    for(let i = 0; i<bd.length;i++){
+        let elemento
+    }
+}*/
